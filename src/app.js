472393232e4a3f6f782798ace4fa99a61e7b4bae
Dave Dunkin <me@davedunkin.com>
2015-01-08 22:13:25 -0700

    Add scene support

--- src/app.js
+++ src/app.js
@@ -44,57 +44,30 @@ if (!accessToken) {
   });
   loadingCard.show();
   
-  var deviceData = null;
+  var scenes = null;
+  var devices = null;
   var authHeaders = { 'Authorization': 'Bearer ' + accessToken};
+  
+  var scenesSectionIndex = 0;
+  var devicesSectionIndex = 1;
 
-  var deviceMenu = new UI.Menu();
-  deviceMenu.on('select', function(e) {
+  var mainMenu = new UI.Menu();
+  mainMenu.on('select', function(e) {
     console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
   
-    var deviceIndex = e.itemIndex;
-    var device = deviceData[deviceIndex];
-    console.log(JSON.stringify(device));
-//     if (device.object_type == 'light_bulb') {
-    if (device.light_bulb_id) {
-      var itemMenu = new UI.Menu({
-        sections: [{
-          title: device.name,
-          items: [{
-            title: 'High',
-          }, {
-            title: 'Medium',
-          }, {
-            title: 'Low',
-          }, {
-            title: 'Off',
-          }]
-        }]
-      });
-      itemMenu.on('select', function(e) {
+    if (e.sectionIndex === scenesSectionIndex) {
+      var sceneIndex = e.itemIndex;
+      var scene = scenes[sceneIndex];
+      console.log(JSON.stringify(scene));
+      if (scene.scene_id) {
         ajax({
-          url: 'https://winkapi.quirky.com/light_bulbs/' + device.light_bulb_id,
-          method: 'put',
+          url: 'https://winkapi.quirky.com/scenes/' + scene.scene_id + '/activate',
+          method: 'post',
           type: 'json',
-          headers: authHeaders,
-          data: {
-            "nonce": (Math.random() * 1000000000).toString(),
-            "desired_state": {
-              "powered": e.itemIndex != 3,
-              "powered_updated_at": Date.now(),
-              "brightness": e.itemIndex === 0 ? 1 :
-              e.itemIndex == 1 ? 0.4 :
-              e.itemIndex == 2 ? 0 :
-              1
-            }
-          }
+          headers: authHeaders
         },
         function(data) {
           console.log(JSON.stringify(data));
-          // update our device data
-          device = data.data;
-          if (device) {
-            deviceData[deviceIndex] = device;
-          }
         },
         function(error) {
           var errorCard = new UI.Card({
@@ -103,37 +76,126 @@ if (!accessToken) {
           });
           errorCard.show();
         });
-      });
-      itemMenu.show();
+      }
+    } else if (e.sectionIndex === scenesSectionIndex) {
+      var deviceIndex = e.itemIndex;
+      var device = devices[deviceIndex];
+      console.log(JSON.stringify(device));
+      if (device.light_bulb_id) {
+        var itemMenu = new UI.Menu({
+          sections: [{
+            title: device.name,
+            items: [{
+              title: 'High',
+            }, {
+              title: 'Medium',
+            }, {
+              title: 'Low',
+            }, {
+              title: 'Off',
+            }]
+          }]
+        });
+        itemMenu.on('select', function(e) {
+          ajax({
+            url: 'https://winkapi.quirky.com/light_bulbs/' + device.light_bulb_id,
+            method: 'put',
+            type: 'json',
+            headers: authHeaders,
+            data: {
+              "nonce": (Math.random() * 1000000000).toString(),
+              "desired_state": {
+                "powered": e.itemIndex != 3,
+                "powered_updated_at": Date.now(),
+                "brightness": e.itemIndex === 0 ? 1 :
+                e.itemIndex == 1 ? 0.4 :
+                e.itemIndex == 2 ? 0 :
+                1
+              }
+            }
+          },
+          function(data) {
+            console.log(JSON.stringify(data));
+            // update our device data
+            device = data.data;
+            if (device) {
+              devices[deviceIndex] = device;
+            }
+          },
+          function(error) {
+            var errorCard = new UI.Card({
+              title: 'Error',
+              body: error
+            });
+            errorCard.show();
+          });
+        });
+        itemMenu.show();
+      }
     }
   });
   
-  // load device data
+  // load scenes
+  ajax({
+      url: 'https://winkapi.quirky.com/users/me/scenes',
+      type: 'json',
+      headers: authHeaders
+    },
+    function(data) {
+      console.log('Got scenes');
+      
+      scenes = data.data;
+          
+      loadingCard.hide();
+  
+      var section = {
+        title: 'Scenes',
+        items: scenes.map(function(obj) {
+          return { title: obj.name };
+        })
+      };
+      mainMenu.section(scenesSectionIndex, section);
+      mainMenu.show();
+    },
+    function(error) {
+      console.log('Error getting scenes: ' + error);
+      
+      loadingCard.hide();
+      
+      var errorCard = new UI.Card({
+        title: 'Error',
+        body: error
+      });
+      errorCard.show();
+    }
+  );
+  
+  // load devices
   ajax({
       url: 'https://winkapi.quirky.com/users/me/wink_devices',
       type: 'json',
       headers: authHeaders
     },
     function(data) {
-      console.log('Got device data');
+      console.log('Got devices');
       
-      deviceData = data.data;
+      devices = data.data;
           
-      loadingCard.hide();
+//       loadingCard.hide();
   
       var section = {
-        title: 'Wink Devices',
-        items: deviceData.map(function(obj) {
+        title: 'Devices',
+        items: devices.map(function(obj) {
           return { title: obj.name };
         })
       };
-      deviceMenu.section(0, section);
-      deviceMenu.show();
+      mainMenu.section(devicesSectionIndex, section);
+      mainMenu.show();
     },
     function(error) {
-      console.log('Error getting device data: ' + error);
+      console.log('Error getting devices: ' + error);
       
-      loadingCard.hide();
+//       loadingCard.hide();
       
       var errorCard = new UI.Card({
         title: 'Error',
